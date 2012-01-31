d1ba43f14f850a9c0e934e91d83d0dd4828fc365
Yutaka HARA <yutaka.hara.gmail.com>
2012-01-31 14:12:37 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Afpray::Application.config.secret_token = '9a11df154a6f4178bd33a990c7cb16186c962a3ae9ed52137640479bcb0e08a2b54c622a5d6dff2038ee4176a63f91e23856c30727cbb1cd5e8c194d0d07025d'
